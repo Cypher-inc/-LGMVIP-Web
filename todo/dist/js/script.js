da2b786b3b3296a50791d05bb65d7a53335814b5
Cypher-inc <victorynorden@gmail.com>
2022-02-07 16:44:42 +0530

    responsive

--- todo/dist/js/script.js
+++ todo/dist/js/script.js
@@ -16,9 +16,9 @@ var AddTask1 = function(){
 
         div1 = document.createElement('div');
         // par.textContent = txt1
-        div1.innerHTML = '<p class="listP"><button class="check" onclick="checkBtn(this.parentNode)">&check;</button>'
-        + txt1
-        +'<button class="close1" onclick="closeBtn(this.parentNode)">&#x2715</button></p>'
+        div1.innerHTML = '<button class="check" onclick="checkBtn(this.parentNode)">&check;</button><p class="listP">'
+        + txt1 +
+        '</p> <button class="close1" onclick="closeBtn(this.parentNode)">&#x2715</button>'
         div1.classList.add('standard')
         el = document.querySelector('.todo-list');
         el.appendChild(div1)
@@ -28,22 +28,25 @@ var AddTask1 = function(){
 }
 addTask.addEventListener('click', AddTask1)
 
-
-
-//Done Task
+//Done Task.
 function checkBtn(div1){
-    div1.classList.add('checked')
+    div1.classList.add('checked-d') //for fade
+
+    var y = div1.getElementsByTagName("*"); 
+    for (var i = 0; i < y.length; i++) {
+      if(y[i].classList.contains('listP')){
+        y[i].classList.add('checked-p') //for line through
+      }
+    }
 }
 
 //Delete Task
 function closeBtn(div1){
-    // div1.classList.add('fall')
     div1.remove()    
 }
 
 //Enter key adds task
 document.addEventListener('keydown', function(e){
-
     if(e.key === 'Enter'){ 
         // console.log('Ent was pressed');
         AddTask1()
